431edaa84bec02701ac19ca1212657b7efc89316
Andrii Stolitnii <andrii@ethflow.io>
2025-12-17 20:24:43 +0100

    fixed .gitignore and removed node_modules

--- TeachBound-host/node_modules/@libp2p/websockets/dist/src/websocket-to-conn.d.ts
+++
@@ -1,9 +0,0 @@
-import type { MultiaddrConnection } from '@libp2p/interface';
-import type { AbstractMultiaddrConnectionInit } from '@libp2p/utils';
-export interface WebSocketMultiaddrConnectionInit extends Omit<AbstractMultiaddrConnectionInit, 'name'> {
-    websocket: WebSocket;
-    maxBufferedAmount?: number;
-    bufferedAmountPollInterval?: number;
-}
-export declare function webSocketToMaConn(init: WebSocketMultiaddrConnectionInit): MultiaddrConnection;
-//# sourceMappingURL=websocket-to-conn.d.ts.map
