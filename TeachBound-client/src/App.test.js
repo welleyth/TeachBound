1acc60b0c7dd7721027952513c18c0c495db8fca
Andrii Stolitnii <andrii@ethflow.io>
2025-12-18 01:25:59 +0100

    Fix Jest tests for P2P/Canvas environment

--- TeachBound-client/src/App.test.js
+++ TeachBound-client/src/App.test.js
@@ -1,8 +1,35 @@
 import { render, screen } from '@testing-library/react';
+
+// Disable P2P in tests even if `.env` is present.
+process.env.REACT_APP_P2P_SIGNALING_ADDR = '';
+process.env.REACT_APP_P2P_HOST = '';
+
+// libp2p is ESM-only and not needed for the React component unit test.
+// Mock the P2P module so Jest doesn't try to resolve libp2p in Node.
+jest.mock('./p2p/startP2P', () => ({
+  addP2PEventListener: () => () => {},
+  getP2PNode: () => null,
+  publishP2PEvent: () => ({}),
+  scheduleStopP2P: () => {},
+  startP2P: () =>
+    Promise.resolve({
+      peerId: { toString: () => 'test-peer' },
+      getConnections: () => []
+    })
+}));
+
+// Canvas relies on HTMLCanvasElement.getContext which is not implemented in jsdom by default.
+jest.mock('./Canvas', () => {
+  const React = require('react');
+  return {
+    __esModule: true,
+    default: React.forwardRef(() => <div data-testid="canvas" />)
+  };
+});
+
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Teach Bound header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/teach bound/i)).toBeInTheDocument();
 });
--- TeachBound-client/src/setupTests.js
+++ TeachBound-client/src/setupTests.js
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// jsdom (Jest) may not provide TextEncoder/TextDecoder globals, but libp2p protocol code uses them.
+// Provide Node's implementations for tests.
+import { TextDecoder, TextEncoder } from 'util';
+
+if (globalThis.TextEncoder == null) globalThis.TextEncoder = TextEncoder;
+if (globalThis.TextDecoder == null) globalThis.TextDecoder = TextDecoder;
